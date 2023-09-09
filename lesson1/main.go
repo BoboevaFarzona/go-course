88e488828d37cf760ecd2a1fc6c589722bb33eb6
Farzona <boboeva1118@gmail.com>
2023-09-09 16:36:36 +0500

    homework1

--- lesson1/main.go
+++ lesson1/main.go
@@ -5,7 +5,15 @@ import (
 )
 
 func main() {
-	fmt.Println(globalVar)
+	var sum float64
+	fmt.Println("Введите сумму в сомони: ")
+	fmt.Scan(&sum)
+	const tjsToUsd = 0.091
+	const tjsToRub = 8.6522
+	usd := sum * tjsToUsd
+	rub := sum * tjsToRub
+	fmt.Printf("%.2f сомони = %.2f доллар\n", sum, usd)
+	fmt.Printf("%.2f сомони = %.2f рубль", sum, rub)
 }
 
 // функция месозем берун аз мейн
